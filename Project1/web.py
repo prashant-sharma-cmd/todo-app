ed7cf62ef9fa32f8f51b35d515a256bb39aa0f45
Admin <prashant.shar51@gmail.com>
2025-06-24 18:18:36 +0545

    New todo addition feature added to web app, Day 19

--- Project1/web.py
+++ Project1/web.py
@@ -3,6 +3,11 @@ import streamlit as st
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo_local = st.session_state["new_todo"] + '\n'
+    todos.append(todo_local)
+    functions.write_todos(todos)
+
 st.title("My Todo App")
 st.subheader("This is my Todo app.")
 st.write("This app is to keep track of your activities.")
@@ -10,5 +15,8 @@ st.write("This app is to keep track of your activities.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Enter a todo")
+st.text_input(label="Add Todo",
+              label_visibility="collapsed",
+              placeholder="Enter a todo",
+              on_change=add_todo, key="new_todo")
 
